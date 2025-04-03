4b9c62fd62c2380a70818cd015e92106411f1147
enzoscarpa-fnk <159703846+enzoscarpa-fnk@users.noreply.github.com>
2025-04-03 17:29:08 +0200

    Added auth with Breeze, Generated users with a factory/seeder, Added a 'Role' field in the users table, Added UserController controller, Added a view for /users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => $this->faker->boolean(10) ? 'admin' : 'user',
         ];
     }
 
